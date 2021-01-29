8dd43abdea51eb8d730cd43cf83e1114cf975d05
Aniket <aam257@njit.edu>
2021-01-29 15:50:45 -0500

    Grouped item names and their respective classes.

---
+++ js-jest/src/group.js
@@ -0,0 +1,13 @@
+// Items grouped as per speciality
+
+import Brie from "./brie";
+import Backstage from "./backstage_pass";
+
+let specialItems = {
+  "Aged Brie": Brie,
+  "Backstage passes": Backstage,
+};
+
+let legendaryItems = ["Sulfuras"];
+
+export { specialItems, legendaryItems };
